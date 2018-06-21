ce7016aa26faeddd5964a13ced2f1ee39f4b1907
SUNGHOON <crheart7@gmail.com>
2018-06-21 14:18:17 +0900

    Initial commit

---
+++ contactsapp/src/components/Search.vue
@@ -0,0 +1,11 @@
+<template>
+    <contactForm mode="search" />
+</template>
+
+<script>
+import SearchForm from './SearchForm.vue';
+export default {
+    name : "searchContact",
+    components : { SearchForm }
+}
+</script>
